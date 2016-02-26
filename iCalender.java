eab099f31f5fd63d8c6c10209cd97cce6508d79f
Blake Larson <blake7@hawaii.edu>
2016-02-25 23:59:40 -1000

    Implemented a command interface instead of hard coding prompts.

--- iCalender.java
+++ iCalender.java
@@ -4,17 +4,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class iCalender {
 	private String version = "VERSION:1.0\n";
 	private String prodid = "PRODID:Team Quinze\n";
 	private String uid = "UID:blake7@hawaii.edu\n";
-	private String startTime = "DTSTART:20160225T16300\n";
-	private String endTime = "DTEND:20160225T16450\n";
-	private String location = "LOCATION:ICSpace 318B\n";
-	private String summary = "SUMMARY:ICS314 TA Meeting\n";
-	private String description = "DESCRIPTION:Deliverable #1 presentation to Amy\n";
+	private String startTime;
+	private String endTime;
+	private String location;
+	private String summary;
+	private String description;
 	private String calBegin = "BEGIN:VCALENDAR\n";
 	private String calEnd = "END:VCALENDAR\n";
 	private String eventBegin = "BEGIN:VEVENT\n";
@@ -23,8 +24,21 @@ public class iCalender {
 	public void iCalender(){
 		//Create an empty constructor.
 	}
+	
+	public static void nameFile() {
+		//Create an instance of iCalender
+		iCalender newCal = new iCalender();
+		
+		//Make a name for the .ics file
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What do you want your file to be called?");
+		String eName = reader.next();
+		
+		//Write a new .ics file with an event name
+		newCal.writeFile(eName);
+	}
 
-	public void write(String name){
+	public void writeFile(String name){
 
 		//Take the input name and buld a file from it with append .ics
         StringBuilder builder = new StringBuilder();
@@ -39,7 +53,9 @@ public class iCalender {
             if (!file.exists()) {
                 file.createNewFile();
             }
-
+            
+            createFile();
+          
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(calBegin);
@@ -57,19 +73,51 @@ public class iCalender {
             bw.write(calEnd);
             
             bw.close();
-
+            
             System.out.println("The .ics file has been created.");
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 	}
 	
+	public void createFile() throws IOException {        
+        Scanner reader = new Scanner(System.in);
+        String date;
+        String time;
+        
+        System.out.println("What is the event?");
+        summary = "SUMMARY:" + reader.nextLine() + "\n";
+        
+        System.out.println("What is the description of the event?");
+        description = "DESCRIPTION:" + reader.nextLine() + "\n";
+        
+        System.out.println("What is the location of the event?");
+        location = "LOCATION:" + reader.nextLine() + "\n";
+        
+        System.out.println("What is the date of the event? (YYYYMMDD)");
+        date = reader.nextLine();
+        if (date.length() < 8) {
+        	throw new IOException("Must be YYYYMMDD!");
+        }
+        
+        System.out.println("What time does the event start? (24hr)");
+        time = reader.nextLine();
+        if (time.length() < 4) {
+        	throw new IOException("Must be 24hr!");
+        }
+        startTime = "DTSTART:" + date + "T" + time + "0"+ "\n";
+
+        System.out.println("What time does the event end? (24 hr)");
+        time = reader.nextLine();
+        if (time.length() < 4) {
+        	throw new IOException("Must be 24 hr!");
+        }
+        endTime = "DTEND:" + date + "T" + time + "0" + "\n";
+	}
+	
 	public static void main(String[] args) {
-		//Create an instance of iCalender
-		iCalender newCal = new iCalender();
-		
-		//Write a new .ics file with a name
-		newCal.write("New-Event");
+		nameFile();
 	}
 }
