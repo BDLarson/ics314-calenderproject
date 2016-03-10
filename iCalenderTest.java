5e1108fc398d8d792dd63dc08c5b4ef2b375f23a
Blake Larson <blake7@hawaii.edu>
2016-03-10 01:29:19 -1000

    Starting test cases

--- iCalenderTest.java
+++ iCalenderTest.java
@@ -6,16 +6,35 @@ import java.io.IOException;
 import org.junit.Test;
 
 public class iCalenderTest {
-	//iCalender cal = new iCalender();
+	iCalender cal = new iCalender();
 	
 	@Test
-	public void testName() throws IOException {
-		iCalender alpha = new iCalender();
+	public void testSummary() throws IOException {
+		assertEquals("", cal.getSummary());
+	}
+	
+	@Test
+	public void testDescription() {
+		assertEquals("", cal.getDescription());
 	}
 	
 	@Test
 	public void testlocation() {
-		iCalender alpha = new iCalender();
-		assertEquals("Seattle", alpha.getLocation());
+		assertEquals("LOCATION:Seattle\n", cal.getLocation());
+	}
+	
+	@Test
+	public void testDate() throws IOException {
+		assertEquals("20160311", cal.getDate());
+	}
+	
+	@Test
+	public void testTime() throws IOException {
+		assertEquals("", cal.getTime());
+	}
+	
+	@Test
+	public void testClassification() {
+		assertEquals("", cal.getClassification());
 	}
 }
