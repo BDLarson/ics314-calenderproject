12e904c014014539f5c7c25e1c21ce17caaa7fa2
Blake Larson <blake7@hawaii.edu>
2016-04-27 15:42:38 -1000

    Finished cleaning the program

--- CalenderMain.java
+++ CalenderMain.java
@@ -8,8 +8,9 @@ public class CalenderMain {
 	    Scanner sc = new Scanner(System.in);
 	    Calender cal = new Calender();
 		System.out.println("What do you want to do? (Please enter a number)" );
-		System.out.println("1. Create new .ics file?");
-		System.out.println("2. Import new .ics file?");
+		System.out.println("1. Create new .ics file.");
+		System.out.println("2. Import new .ics file.");
+		System.out.println("3. Exit the program.");
 		System.out.print(">>>");
 	    String next = sc.nextLine();
 
@@ -23,7 +24,7 @@ public class CalenderMain {
 			cal.addPreCreated(); //Start importing files
 			cal.sortEvents(); //Start sorting files
 			cal.printAllEvents(); //Print out all events in order by start time
-			//System.out.println("Nothing programmed yet.");
+			cal.getGreatCircleDistance();
 		} 
 		System.out.println("Thanks! Exiting program!");	}
 }
